0f46591a36daf7e010461acf4673f7e181225358
TD262 <tdd26294@gmail.com>
2026-04-23 01:26:09 -0500

    Changed CORS policy and created gitignore

--- api/analyze.js
+++ api/analyze.js
@@ -8,7 +8,7 @@ export const config = {
 };
 
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://checkmycontract.co');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
